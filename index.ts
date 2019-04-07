4e5e1330e85676063e79908c0174268025ee5bd1
Harminder virk <virk.officials@gmail.com>
2019-04-07 09:18:28 +0530

    feat: register provider

---
+++ index.ts
@@ -0,0 +1,12 @@
+/**
+ * @relayin/base-provider
+ *
+ * (c) Harminder Virk <harminder.virk@relay.in>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+export interface RelayHelpersContract {
+  exec<T extends any> (fn: Promise<T>): Promise<[Error | null, T | null]>,
+}
---
+++ providers/BaseProvider.ts
@@ -0,0 +1,37 @@
+/**
+ * @relayin/base-provider
+ *
+ * (c) Harminder Virk <harminder.virk@relay.in>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+import * as helpers from '../src/Helpers'
+import * as rules from '../src/Validator'
+
+export default class BaseProvider {
+  constructor (public container) {
+  }
+
+  public async boot () {
+    /**
+     * Extend helpers by adding methods to it
+     */
+    this.container.with(['Adonis/Src/Helpers'], (Helpers) => {
+      Object.keys(helpers).forEach((fn) => {
+        Helpers[fn] = helpers[fn]
+      })
+    })
+
+    /**
+     * Extend indicative, if it exists
+     */
+    try {
+      const { extend } = require('indicative')
+      Object.keys(rules).forEach((rule) => extend(rule, rules[rule]))
+    } catch (error) {
+      // ignore if indicative is not installed
+    }
+  }
+}
